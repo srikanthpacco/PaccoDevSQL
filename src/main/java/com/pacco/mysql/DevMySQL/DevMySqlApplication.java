dc5a8008e30ad1e9659d4719646e0b6f5e3ae173
Srikanth Reddy Dubudu <srikanth.reddy@pacc0.com>
2022-10-24 16:28:34 -0400

    first commit

---
+++ src/main/java/com/pacco/mysql/DevMySQL/DevMySqlApplication.java
@@ -0,0 +1,13 @@
+package com.pacco.mysql.DevMySQL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevMySqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevMySqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pacco/mysql/DevMySQL/DevMySqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pacco.mysql.DevMySQL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevMySqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
